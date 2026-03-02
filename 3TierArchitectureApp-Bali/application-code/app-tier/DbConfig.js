9af25c6c0303ea899398024403f4b70900e56d7c
bali895 <guptabalistar1998@gmail.com>
2026-03-02 16:21:20 +0530

    Delete 3TierArchitectureApp-Bali directory

--- 3TierArchitectureApp-Bali/application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database2.c83giggym06r.us-east-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'Admin12345',
-    DB_DATABASE : 'webappdb'
-});
