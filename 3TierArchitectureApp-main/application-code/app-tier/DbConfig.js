931f5874880425ceedccdb566ef1661ece638b3e
bali895 <guptabalistar1998@gmail.com>
2026-03-03 12:36:40 +0530

    Add files via upload

---
+++ 3TierArchitectureApp-main/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database1.c83giggym06r.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin12345',
+    DB_DATABASE : 'webappdb'
+});
